29fd316d605b2161fb02b24be1f0478397645abe
Matthew Wright <matthew@smallhelm.com>
2015-06-17 07:55:31 -0600

    refactoring

--- index.js
+++ index.js
@@ -1,82 +1,68 @@
 var through2 = require('through2');
 
-module.exports = function(){
-  var findMatchingRule = function(text){
-    var i;
-    for(i=0; i<rules.length; i++){
-      if(rules[i][0].test(text)){
-        return rules[i];
-      }
-    }
-    return undefined;
-  };
-  var findMaxIndex = function(text){
-    var i;
-    var buf;
-    var rule;
-    var found_one_yet = false;
-    for(i=0; i<text.length; i++){
-      buf = text.substring(0, i + 1);
-      rule = findMatchingRule(buf);
-      if(rule){
-        found_one_yet = true;
-      }else{
-        if(found_one_yet){
-          return i;
-        }
-      }
-    }
-    return found_one_yet ? text.length : 0;
-  };
+var findMatchingRule = function(rules, text){
+  var i;
+  for(i=0; i<rules.length; i++)
+    if(rules[i].regex.test(text))
+      return rules[i];
+  return undefined;
+};
 
+var findMaxIndexAndRule = function(rules, text){
+  var i, rule, last_matching_rule;
+  for(i=0; i<text.length; i++){
+    rule = findMatchingRule(rules, text.substring(0, i + 1));
+    if(rule)
+      last_matching_rule = rule;
+    else if(last_matching_rule)
+      return {max_index: i, rule: last_matching_rule};
+  }
+  return last_matching_rule ? {max_index: text.length, rule: last_matching_rule} : {max_index: 0, rule: undefined};
+};
+
+module.exports = function(){
   var buffer = '';
   var rules = [];
   var line = 1;
   var col = 1;
 
+  var onToken = function(src, type){
+    token_stream.push({
+      type: type,
+      src: src,
+      line: line,
+      col: col
+    });
+    var lines = src.split("\n");
+    line += lines.length - 1;
+    col = (lines.length > 1 ? 1 : col) + lines[lines.length - 1].length;
+  };
+
   var token_stream = through2.obj(function(chunck, enc, done){
     var str = buffer + chunck.toString();
-    buffer = '';
-    var max_index = findMaxIndex(str);
-    while(max_index > 0 && max_index !== str.length){
-      var src = str.substring(0, max_index);
-      this.push({
-        type: findMatchingRule(src)[1],
-        src: src,
-        line: line,
-        col: col
-      });
-      var lines = src.split("\n");
-      line += lines.length - 1;
-      if(lines.length > 1){
-        col = 1;
-      }
-      col += lines[lines.length - 1].length;
+    var m = findMaxIndexAndRule(rules, str);
+    while(m.rule && m.max_index !== str.length){
+      onToken(str.substring(0, m.max_index), m.rule.type);
 
-      str = str.substring(max_index);
-      max_index = findMaxIndex(str);
+      //now find the next token
+      str = str.substring(m.max_index);
+      m = findMaxIndexAndRule(rules, str);
     }
-    buffer += str;
+    buffer = str;
     done();
   }, function(done){
-    if(buffer.length > 0){
-      var rule = findMatchingRule(buffer);
-      if(!rule){
-        done(new Error('unable to tokenize: ' + buffer));
-        return;
-      }
+    if(buffer.length === 0)
+      return done();
 
-      this.push({
-        type: rule[1],
-        src: buffer,
-        line: line,
-        col: col
-      });
-    }
+    var rule = findMatchingRule(rules, buffer);
+    if(!rule)
+      return done(new Error('unable to tokenize: ' + buffer));
+
+    onToken(buffer, rule.type);
     done();
   });
   token_stream.addRule = function(regex, type){
-    rules.push([regex, type]);
+    rules.push({regex: regex, type: type});
   };
   return token_stream;
 };
